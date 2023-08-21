ccdf4f95443dcb5bdfd2d775522ea4ec37a6d6e2
mvanasselt <m.vanasselt@intergamma.nl>
2023-08-21 14:40:29 +0200

    first setup

---
+++ script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const productListContainer = document.querySelector(".product-list");
+const wishListHeader = document.getElementById("wishlist-header");
+const sidePanel = document.getElementById("sidepanel");
+const wishListContainer = document.getElementById("wishlist-container");
+const sidePanelClose = document.getElementById("sidepanel-close");
+
+const renderProduct = function (product) {
+  const html = `
+    <div class="product-tile" data-product-id="${product.productid}">
+        <img class="product-image" src="${product.image}" alt="product image">
+        <span class="wishlist-icon"> ♡ </span>
+        <div class="product-info">
+          <h3 class="product-name">${product.name}</h3>
+          <div class="product-detail">
+            <span class="product-description">${product.detail}</span>
+            <span class="product-price">€ ${product.price},-</span>
+          </div>
+        </div>
+    `;
+  productListContainer.insertAdjacentHTML("beforeend", html);
+};
+
+const products = () =>
+  fetch("products.json")
+    .then((response) => response.json())
+    .then(function (data) {
+      for (let product of data.products) {
+        renderProduct(product);
+      }
+    });
+
+products();
+
+productListContainer.addEventListener("click", function (event) {
+  const wishListIcon = event.target;
+  const productTile = wishListIcon.parentElement;
+  if (wishListIcon.classList.contains("wishlist-icon"))
+    wishListContainer.appendChild(productTile.cloneNode(true));
+});
+
+wishListContainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("wishlist-icon")) {
+    event.target.parentElement.remove();
+  }
+});
+
+wishListHeader.addEventListener("click", function () {
+  sidePanel.classList.toggle("show");
+});
+
+sidePanelClose.addEventListener("click", function () {
+  sidePanel.classList.remove("show");
+});
