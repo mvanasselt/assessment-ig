b45e1a0c3283ddfe657ee10feab677555280bd03
mvanasselt <m.vanasselt@intergamma.nl>
2023-08-23 14:18:24 +0200

    counter disabled at 0 and counter working on wishlist

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ const renderProduct = function (product) {
         <div class="product-info">
         <h3 class="product-name">${product.name}</h3>
         <div>
-          <button class="counter-minus no-active">-</button>
-          <input class="counter-display value=0"></input>
+          <button class="counter-minus" disabled>-</button>
+          <input class="counter-display value=0" placeholder="0"></input>
           <button class="counter-plus">+</button>
         </div>
         <div class="product-detail">
@@ -38,10 +38,10 @@ const products = () =>
 
 products();
 
-productListContainer.addEventListener("click", function (event) {
+const productCounter = function (event){
   const clickedItem = event.target;
-  const productTile = clickedItem.parentElement;
-  const counterDisplayElem = productTile.querySelector(".counter-display");
+  const counterDisplayElem = clickedItem.parentElement.querySelector(".counter-display");
+  const counterMinus = clickedItem.parentElement.querySelector(".counter-minus");
   let count = counterDisplayElem.value;
 
   if (clickedItem.classList.contains("counter-plus")) {
@@ -51,15 +51,26 @@ productListContainer.addEventListener("click", function (event) {
     count--;
     counterDisplayElem.value = count;
   }
+  counterMinus.disabled = (counterDisplayElem.value > 0) ? false : true;
+}
+
+
+
+productListContainer.addEventListener("click", function (event) {
+  const clickedItem = event.target;
+  const productTile = clickedItem.parentElement;
 
   if (clickedItem.classList.contains("wishlist-icon"))
     wishListContainer.appendChild(productTile.cloneNode(true));
+
+  productCounter(event);
 });
 
 wishListContainer.addEventListener("click", function (event) {
   if (event.target.classList.contains("wishlist-icon")) {
     event.target.parentElement.remove();
   }
+  productCounter(event);
 });
 
 wishListHeader.addEventListener("click", function () {
