3bcfa7da28dd3e815e7c11a16be85aebf8c0dd5e
mvanasselt <m.vanasselt@intergamma.nl>
2023-09-12 16:26:51 +0200

    add and remove items from wishlist

--- script.js
+++ script.js
@@ -5,19 +5,15 @@ const wishListHeader = document.getElementById("wishlist-header");
 const sidePanel = document.getElementById("sidepanel");
 const wishListContainer = document.getElementById("wishlist-container");
 const sidePanelClose = document.getElementById("sidepanel-close");
+const wishList = JSON.parse(localStorage.getItem("wishlist"));
 
-const renderProduct = function (product) {
+const renderProductPage = function (product) {
   const html = `
     <div class="product-tile" data-product-id="${product.productid}">
         <img class="product-image" src="${product.image}" alt="product image">
-        <img class="wishlist-icon" src="images/heart.svg" alt="wishlist-icon">
+        <button class="add-to-wishlist" type="button">Add to wishlist</button>
         <div class="product-info">
         <h3 class="product-name">${product.name}</h3>
-        <div>
-          <button class="counter-minus" disabled>-</button>
-          <input class="counter-display" value=0 placeholder="0"></input>
-          <button class="counter-plus">+</button>
-        </div>
         <div class="product-detail">
         <span class="product-description">${product.detail}</span>
         <span class="product-price">€ ${product.price},-</span>
@@ -27,81 +23,102 @@ const renderProduct = function (product) {
   productListContainer.insertAdjacentHTML("beforeend", html);
 };
 
+const renderProductWishlist = function (product) {
+  const html = `
+    <div class="product-tile" data-product-id="${product.productid}">
+        <img class="product-image" src="${product.image}" alt="product image">
+        <button class="add-to-wishlist" type="button">Remove from wishlist</button>
+        <h3 class="product-name">${product.name}</h3>
+        <div class="product-detail">
+        <span class="product-price">€ ${product.price},-</span>
+        </div>
+        </div>
+    `;
+  wishListContainer.insertAdjacentHTML("beforeend", html);
+};
+
 const products = () =>
   fetch("products.json")
     .then((response) => response.json())
     .then(function (data) {
       for (let product of data.products) {
-        renderProduct(product);
+        renderProductPage(product);
       }
     });
 
 products();
 
-const productCounter = function (clickedItem) {
-  const counterDisplayElem = clickedItem.parentElement.querySelector(".counter-display");
-  const counterMinus = clickedItem.parentElement.querySelector(".counter-minus");
-  let count = counterDisplayElem.value;
-
-  if (clickedItem.classList.contains("counter-plus")) {
-    count++;
-    counterDisplayElem.value = count;
-  } else if (clickedItem.classList.contains("counter-minus")) {
-    count--;
-    counterDisplayElem.value = count;
+const addToWishList = function (product) {
+  if (!localStorage.getItem("wishlist")) {
+    localStorage.setItem("wishlist", JSON.stringify([]));
   }
-  counterMinus.disabled = counterDisplayElem.value > 0 ? false : true;
-};
 
-
-const checkProductInWishlist = function (existingElements, productID) {
-  for (const existingElement of existingElements) {
-    let elementExists = existingElement.getAttribute("data-product-id") === productID ? true : false;
+  if (!wishList.includes(product)) {
+    wishList.push(product);
+    localStorage.setItem("wishlist", JSON.stringify(wishList));
   }
 };
 
-const updateCounterWishlist = function (clickedItem, existingElements, productID) {
-  for (const existingElement of existingElements) {
-    if (existingElement.getAttribute("data-product-id") === productID) {
-      const wishListCounter = existingElement.querySelector(".counter-display");
-      const productCounter = clickedItem.parentElement.querySelector(".counter-display");
-      wishListCounter.value = Number(wishListCounter.value) + Number(productCounter.value);
-    }
-  }
+const removeFromWishlist = function (product) {
+  const wishListIndex = wishList.indexOf(product);
+  wishList.splice(wishListIndex, 1);
+  localStorage.setItem("wishlist", JSON.stringify(wishList));
+  displayWishlist();
+};
+
+const displayWishlist = function () {
+  const wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+  wishListContainer.innerHTML = "";
+  wishlist.forEach((item) => {
+    fetch("products.json")
+      .then((response) => response.json())
+      .then(function (data) {
+        const dataArray = Array.from(data.products);
+        const productIndex = dataArray.findIndex((product) => product.name === item);
+        renderProductWishlist(dataArray[productIndex]);
+      });
+  });
 };
 
 productListContainer.addEventListener("click", function (event) {
-  const clickedItem = event.target;
-  const productTile = clickedItem.parentElement;
-  const productID = productTile.getAttribute("data-product-id");
-  const existingElements = wishListContainer.querySelectorAll("[data-product-id]");
-  const wishListIcon = clickedItem.parentElement.querySelector(".wishlist-icon")
+  const productTile = event.target.closest(".product-tile");
+  const productName = productTile.querySelector(".product-name").textContent;
 
-  productCounter(clickedItem);
-  checkProductInWishlist(existingElements, productID);
-
-  if (clickedItem.classList.contains("wishlist-icon")) {
-    wishListIcon.classList.add("clicked");
-    if (!elementExists) {
-      wishListContainer.appendChild(productTile.cloneNode(true));
-    } else if (elementExists) {
-      updateCounterWishlist(clickedItem, existingElements, productID);
-    }
-    productTile.querySelector(".counter-display").value = 0;
+  if (event.target.classList.contains("add-to-wishlist")) {
+    addToWishList(productName);
+    displayWishlist();
   }
 });
 
 wishListContainer.addEventListener("click", function (event) {
-  if (event.target.classList.contains("wishlist-icon")) {
-    event.target.parentElement.remove();
+  if (event.target.classList.contains("add-to-wishlist")) {
+    const productTile = event.target.closest(".product-tile");
+    const productName = productTile.querySelector(".product-name").textContent;
+
+    removeFromWishlist(productName);
   }
-  productCounter(event.target);
 });
 
 wishListHeader.addEventListener("click", function () {
   sidePanel.classList.toggle("show");
+  displayWishlist();
 });
 
 sidePanelClose.addEventListener("click", function () {
   sidePanel.classList.remove("show");
 });
+
+// const productCounter = function (clickedItem) {
+//   const counterDisplayElem = clickedItem.parentElement.querySelector(".counter-display");
+//   const counterMinus = clickedItem.parentElement.querySelector(".counter-minus");
+//   let count = counterDisplayElem.value;
+
+//   if (clickedItem.classList.contains("counter-plus")) {
+//     count++;
+//     counterDisplayElem.value = count;
+//   } else if (clickedItem.classList.contains("counter-minus")) {
+//     count--;
+//     counterDisplayElem.value = count;
+//   }
+//   counterMinus.disabled = counterDisplayElem.value > 0 ? false : true;
+// };
