eb1b18c323ff7f35fd782742cf3e31528d9b8fda
mvanasselt <m.vanasselt@intergamma.nl>
2023-09-14 09:37:27 +0200

    asynch await fetch json

--- script.js
+++ script.js
@@ -28,6 +28,9 @@ const renderProductWishlist = function (product) {
     <div class="product-tile" data-product-id="${product.productid}">
         <img class="product-image" src="${product.image}" alt="product image">
         <button class="add-to-wishlist" type="button">Remove from wishlist</button>
+        <button class="counter-minus" type="button">-</button>
+        <input class="counter-display" type="text" value="1">
+        <button class="counter-plus" type="button">+</button>
         <h3 class="product-name">${product.name}</h3>
         <div class="product-detail">
         <span class="product-price">€ ${product.price},-</span>
@@ -37,16 +40,66 @@ const renderProductWishlist = function (product) {
   wishListContainer.insertAdjacentHTML("beforeend", html);
 };
 
-const products = () =>
-  fetch("products.json")
-    .then((response) => response.json())
-    .then(function (data) {
-      for (let product of data.products) {
-        renderProductPage(product);
-      }
-    });
+let productData = null;
+
+const fetchProductData = async () => {
+  if (productData === null) {
+    const response = await fetch("products.json");
+    const data = await response.json();
+    productData = data.products;
+  }
+};
+
+const products = async () => {
+  await fetchProductData();
+
+  productData.forEach((product) => {
+    renderProductPage(product);
+  });
+};
+
+const displayWishlist = async () => {
+  await fetchProductData();
+
+  const wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+  wishListContainer.innerHTML = "";
+
+  const dataArray = Array.from(productData);
+
+  wishlist.forEach((item) => {
+    const productIndex = dataArray.findIndex((product) => product.name === item);
+    renderProductWishlist(dataArray[productIndex]);
+  });
+};
 
 products();
+
+// const products = () =>
+//   fetch("products.json")
+//     .then((response) => response.json())
+//     .then(function (data) {
+//       for (let product of data.products) {
+//         renderProductPage(product);
+//       }
+//     });
+
+// products();
+
+// const displayWishlist = function () {
+//   const wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+//   wishListContainer.innerHTML = "";
+//   fetch("products.json")
+//     .then((response) => response.json())
+//     .then(function (data) {
+//       const dataArray = Array.from(data.products);
+//       dataArray.forEach((product) => (product.quantity = 0));
+//       wishlist.forEach((item) => {
+//         const productIndex = dataArray.findIndex((product) => product.name === item);
+//         renderProductWishlist(dataArray[productIndex]);
+//       });
+//       console.log(dataArray);
+//     });
+// };
 
 const addToWishList = function (product) {
   if (!localStorage.getItem("wishlist")) {
@@ -56,6 +109,7 @@ const addToWishList = function (product) {
   if (!wishList.includes(product)) {
     wishList.push(product);
     localStorage.setItem("wishlist", JSON.stringify(wishList));
+    displayWishlist();
   }
 };
 
@@ -66,37 +120,37 @@ const removeFromWishlist = function (product) {
   displayWishlist();
 };
 
-const displayWishlist = function () {
-  const wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
-  wishListContainer.innerHTML = "";
-  wishlist.forEach((item) => {
-    fetch("products.json")
-      .then((response) => response.json())
-      .then(function (data) {
-        const dataArray = Array.from(data.products);
-        const productIndex = dataArray.findIndex((product) => product.name === item);
-        renderProductWishlist(dataArray[productIndex]);
-      });
-  });
+localStorage.setItem("count", JSON.stringify(0));
+let totalAmount = JSON.parse(localStorage.getItem("count"));
+const productCounter = function (product) {
+  const counterDisplayElem = product.parentElement.querySelector(".counter-display");
+  const counterMinus = product.parentElement.querySelector(".counter-minus");
+  let count = counterDisplayElem.value;
+  if (product.classList.contains("counter-plus")) {
+    count++;
+    counterDisplayElem.value = count;
+  } else if (product.classList.contains("counter-minus")) {
+    count--;
+    counterDisplayElem.value = count;
+  }
+  counterMinus.disabled = counterDisplayElem.value > 0 ? false : true;
 };
 
 productListContainer.addEventListener("click", function (event) {
   const productTile = event.target.closest(".product-tile");
   const productName = productTile.querySelector(".product-name").textContent;
-
   if (event.target.classList.contains("add-to-wishlist")) {
     addToWishList(productName);
-    displayWishlist();
   }
 });
 
 wishListContainer.addEventListener("click", function (event) {
+  const productTile = event.target.closest(".product-tile");
+  const productName = productTile.querySelector(".product-name").textContent;
   if (event.target.classList.contains("add-to-wishlist")) {
-    const productTile = event.target.closest(".product-tile");
-    const productName = productTile.querySelector(".product-name").textContent;
-
     removeFromWishlist(productName);
   }
+  productCounter(event.target);
 });
 
 wishListHeader.addEventListener("click", function () {
@@ -107,18 +161,3 @@ wishListHeader.addEventListener("click", function () {
 sidePanelClose.addEventListener("click", function () {
   sidePanel.classList.remove("show");
 });
-
-// const productCounter = function (clickedItem) {
-//   const counterDisplayElem = clickedItem.parentElement.querySelector(".counter-display");
-//   const counterMinus = clickedItem.parentElement.querySelector(".counter-minus");
-//   let count = counterDisplayElem.value;
-
-//   if (clickedItem.classList.contains("counter-plus")) {
-//     count++;
-//     counterDisplayElem.value = count;
-//   } else if (clickedItem.classList.contains("counter-minus")) {
-//     count--;
-//     counterDisplayElem.value = count;
-//   }
-//   counterMinus.disabled = counterDisplayElem.value > 0 ? false : true;
-// };
